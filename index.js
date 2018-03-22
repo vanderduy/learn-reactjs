73414f9a7e2e8d8cf69a554e4f55a76fae5349e5
duy <nguyenkhuongduy160996@gmail.com>
2018-03-22 17:20:35 +0700

    test gitignore

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send({bye:"lol"})
-})
+const passport = require('passport')
+const googleStrategy = require('passport-google-oauth20').Strategy 
+//https://console.developers.google.com
+
+//clinetID 435164350783-bofsr4rolh7vebvcpfktl65o11nhhtpp.apps.googleusercontent.com
+//clinetSecret kdQ7AxCXoyIve3K-mX1Vi27A
+
+passport.use(new googleStrategy())
+
 
 const PORT = process.env.PORT || 5000
 
