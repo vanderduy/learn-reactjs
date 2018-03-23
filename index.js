8a72da932c9eafd11fc454360eb7afb8ad27a37d
duy <nguyenkhuongduy160996@gmail.com>
2018-03-23 15:21:20 +0700

    get inf from user

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-const passport = require('passport')
-const googleStrategy = require('passport-google-oauth20').Strategy 
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth20").Strategy;
 //https://console.developers.google.com
+const key = require("./config/key");
 
-//clinetID 435164350783-bofsr4rolh7vebvcpfktl65o11nhhtpp.apps.googleusercontent.com
-//clinetSecret kdQ7AxCXoyIve3K-mX1Vi27A
+passport.use(
+  new googleStrategy(
+    {
+      clientID: key.googleClientID,
+      clientSecret: key.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken , profile, done )=> {
+      console.log('accessToken :' , accessToken);
+      console.log('refreshToken :' , refreshToken);
+      console.log('profile :' , profile);
+    }
+  )
+);
 
-passport.use(new googleStrategy())
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
+app.get('/auth/google/callback', passport.authenticate('google'))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT)
+app.listen(PORT);
