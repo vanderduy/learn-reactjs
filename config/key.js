40ac19e06d5df4bcc42b394b1bc16537bb6303aa
duy <nguyenkhuongduy160996@gmail.com>
2018-03-27 13:50:58 +0700

    test

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV == 'production'){
     //we are in production- return the prod set of keys
     module.exports = require('./prod')
 } else{
