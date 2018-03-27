e3fccd26732fdabe04d7126e49ea46234fa7fe82
duy <nguyenkhuongduy160996@gmail.com>
2018-03-27 11:02:37 +0700

    cookie session

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const mongoose =  require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require("passport");
 const key  =  require('./config/key')
 
 
@@ -9,6 +11,15 @@ mongoose.connect(key.mongoURI)
 
 //https://console.developers.google.com
 const app = express();
+
+app.use(cookieSession({
+    maxAge : 30*24*24*60*1000,
+    keys :[key.cookieKey]
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 require('../server/services/passport')
 require('./routes/authRoute')(app)
 const PORT = process.env.PORT || 5000
--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,20 @@
 const passport = require("passport");
 const googleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
+
 const User = mongoose.model("users");
 const key = require("../config/key");
+
+passport.serializeUser((user,done)=>{
+  done(null,user.id)
+})
+
+passport.deserializeUser((id,done)=>{
+  User.findById(id)
+  .then(user=>{
+    done(null,user)
+  })
+})
 
 passport.use(
   new googleStrategy(
