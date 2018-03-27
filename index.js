dc2e536e9be6877c50430119900c62d999a36895
duy <nguyenkhuongduy160996@gmail.com>
2018-03-27 14:06:39 +0700

    test

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const express = require("express")
-const mongoose =  require('mongoose')
-const cookieSession = require('cookie-session')
-const passport = require("passport");
-const key  =  require('./config/key')
+// const mongoose =  require('mongoose')
+// const cookieSession = require('cookie-session')
+// const passport = require("passport");
+// const key  =  require('./config/key')
 
 
-require('./models/User')
+// require('./models/User')
 
-mongoose.connect(key.mongoURI)
+// mongoose.connect(key.mongoURI)
 
 //https://console.developers.google.com
 const app = express();
 
-app.use(cookieSession({
-    maxAge : 30*24*24*60*1000,
-    keys :[key.cookieKey]
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(cookieSession({
+//     maxAge : 30*24*24*60*1000,
+//     keys :[key.cookieKey]
+// }))
+// app.use(passport.initialize())
+// app.use(passport.session())
 
 
-require('../server/services/passport')
-require('./routes/authRoute')(app)
+// require('../server/services/passport')
+// require('./routes/authRoute')(app)
+
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT);
