e03e4a593b10d281a4f5bc7d01bf82bcf3b13111
duy <nguyenkhuongduy160996@gmail.com>
2018-03-27 14:12:31 +0700

    test

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+//dev.js - don't commit
+
+module.exports={
+    googleClientID : '435164350783-bofsr4rolh7vebvcpfktl65o11nhhtpp.apps.googleusercontent.com',
+    googleClientSecret: 'kdQ7AxCXoyIve3K-mX1Vi27A',
+    mongoURI: 'mongodb://_username:kingppro@ds123259.mlab.com:23259/emaily-dev-learn',
+    cookieKey :'akhfbksajbfkjsabkfjsakjfsakj'
+}
